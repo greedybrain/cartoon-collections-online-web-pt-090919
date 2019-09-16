6934b891b4c87082e649f2a650f1444181c86ecc
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-16 12:51:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarf_names)
   end
 end
 
-def summon_captain_plane(planeteer_calls)
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |call|
     "#{call.capitalize}!"
   end
