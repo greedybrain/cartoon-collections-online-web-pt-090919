801f593683d9ba6afb80484dcff45ab93dc3c40a
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-16 12:48:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
+  dwarf_names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
