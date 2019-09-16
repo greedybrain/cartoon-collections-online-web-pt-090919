11e1a6177421c0d5925eeb6d8790ac5d72c7a957
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-16 13:02:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese(strings)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    strings.include?(cheese) ? cheese : nil
+    strings.include?(cheese) ? return cheese : return nil
   end
   
 end
